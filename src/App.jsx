e7a7f3bb86d747b4fc90e4f3f530658bc19b6b3a
BalaPriyaMS <balapriya@intellicar.in>
2024-06-26 17:54:53 +0530

    updating the value

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,23 @@
+import { useEffect } from "react";
 import "./App.css";
+import { useState } from "react";
 
 function App() {
-  const seconds = 60;
+  const [seconds, setSeconds] = useState(1);
+  useEffect(() => {
+    let id = null;
+    id = setInterval(() => {
+      setSeconds((prev) => prev - 1);
+    }, 1000);
+    if (seconds === 0) clearInterval(id);
+    return () => clearInterval(id);
+  }, [seconds]);
 
   return (
     <div style={{ padding: "20px", textAlign: "center" }}>
       <h1>Countdown Timer</h1>
       <div style={{ fontSize: "48px", margin: "20px" }}>{seconds}s</div>
-      {seconds === 0 && <div>Time's up!</div>}
+      {seconds === 0 && <div>Times up!</div>}
     </div>
   );
 }
